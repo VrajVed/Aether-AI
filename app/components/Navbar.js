55031bfad2a53dfc830234dcc2d90f1c0e7efc1f
Vraj Ved <vraj.a.ved@gmail.com>
2025-07-21 22:57:25 +0530

    added pixy's div

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -4,11 +4,12 @@ const Navbar = () => {
   return (
     <div className='justify-center flex mt-2 '>
         <div className=''></div>
-          <ul className='justify-between shadow-2xl/20  shadow-green-500 flex p-4 w-xl rounded-md border-2 border-neutral-400/20  text-neutral-200 backdrop:blur-[1px] bg-neutral-400/20 transition-0.3'>
+          <ul className='justify-between flex p-4 w-xl rounded-md border-2 border-neutral-400/20  text-neutral-200 backdrop:blur-[1px] bg-neutral-400/20 transition-0.3'>
             <li className='hover:text-neutral-50 cursor-pointer'>Features</li>
-            <li>Testimonials</li>
-            <li>Pricing</li>
-            <li>About us</li>
+            <li className='hover:text-neutral-50 cursor-pointer'>Testimonials</li>
+            <li className='hover:text-neutral-50 cursor-pointer'>Pricing</li>
+            <li className='hover:text-neutral-50 cursor-pointer'>About us</li>
+            <button className='hover:bg-green-900 text-white cursor-pointer pl-2 pr-2 bg-green-700 rounded-md'>Get Started</button>
         </ul>
     </div>
   )
--- app/page.js
+++ app/page.js
@@ -3,7 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#48bb78_100%)]"></div>
+      <div
+        className="rounded-full pointer-events-none z-0 bg-[#00FF443F]
+                          sm:w-[408px] sm:h-[222px] sm:top-[-99px]
+                          md:w-[675px] md:h-[368px] md:top-[-300px] md:right-[450px]
+                          absolute blur-[160px]"
+      ></div>
       <div className="justify-center flex text-white mt-40 items-center flex-col">
         <h1 className="font-bold font text-5xl ">Your AI, Visualized.</h1>
         <p className="p-3 font-thin">Real Time Training, Clear Metrics, Smart Results</p>
@@ -14,11 +19,11 @@ export default function Home() {
           </p>
         </div>
         <div>
-          <button className="cursor-pointer p-3 mr-25 border-neutral-100 border-1 rounded-md">Try Aether for Free</button>
+          <button className="cursor-pointer p-3 mr-25 border-neutral-100 border-1 rounded-md hover:bg-neutral-500/50">Try Aether for Free</button>
           <button className="cursor-pointer p-3 rounded-md text-neutral-950 bg-neutral-50 font-semibold">See How it Works</button>
         </div>
         <div className="bg-white p-20 mt-10 rounded-2xl">
-          2
+          
         </div>
       </div>
     </>
